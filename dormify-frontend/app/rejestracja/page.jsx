e2e92b55955a43edba7b6a73856c07cee83e6514
Oskar Bielecki <bieleckioskar@gmail.com>
2025-01-02 17:48:30 +0100

    Rejestracja #1

--- dormify-frontend/app/rejestracja/page.jsx
+++ dormify-frontend/app/rejestracja/page.jsx
@@ -1,12 +1,73 @@
+'use client';
 import React from 'react';
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { MdOutlinePassword } from 'react-icons/md';
+import { loginUser } from '@/lib/auth';
 
-const page = () => {
+export default function User() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const data = await loginUser(login, password);
+      localStorage.setItem('access', data.access);
+      localStorage.setItem('refresh', data.refresh);
+      alert('Logged in successfully!');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col items-center justify-center text-white">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#001_60%,#61e_100%)]"></div>
-      page
+
+      <div className="bg-violet-950 w-[400px] h-[600px] bg-opacity-50 rounded-xl border-t-4 flex flex-col items-center relative">
+        <h1 className="text-3xl mt-9 opacity-70">Logowanie</h1>
+        <form onSubmit={handleSubmit} className="gap-8 flex flex-col mt-32 ">
+          <div className="flex flex-row justify-between gap-2 items-center">
+            <label>
+              <FaUser />
+            </label>
+            <input
+              type="login"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+              required
+              className="rounded-lg pl-2 text-black"
+              placeholder="E-mail"
+            />
+          </div>
+          <div className="flex flex-row justify-between gap-2 items-center">
+            <label>
+              <MdOutlinePassword />
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="rounded-lg pl-2 text-black"
+              placeholder="Hasło"
+            />
+          </div>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <div className="absolute bottom-0 left-0 w-full flex flex-col items-center mb-16">
+            <button
+              type="submit"
+              className=" items-center text-center bg-white text-violet-950 font-bold py-3 px-6 text-2xl rounded-lg"
+            >
+              Zaloguj
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-};
-
-export default page;
+}
