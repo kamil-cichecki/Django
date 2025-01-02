d68b2203277f77f1ac2896850d866f4e8d7bb283
Oskar Bielecki <bieleckioskar@gmail.com>
2025-01-02 17:16:01 +0100

    Branch

--- dormify-frontend/app/rejestracja/page.jsx
+++ dormify-frontend/app/rejestracja/page.jsx
@@ -4,7 +4,6 @@ const page = () => {
   return (
     <div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#001_60%,#61e_100%)]"></div>
-      page
     </div>
   );
 };
