686bb9da6bae7dcb4a9b86e8d208a48fe84b58c5
Kamil Cichecki <kamil.cichecki00@gmail.com>
2025-01-23 19:01:55 +0100

    Room update

--- Dormify/DormifyApp/views/Room/room_views.py
+++ Dormify/DormifyApp/views/Room/room_views.py
@@ -32,7 +32,6 @@ def get_latest_rooms_by_dormitory(request, dormitory_id):
         except Dormitory.DoesNotExist:
             return JsonResponse({"error": "Akademik o podanym ID nie istnieje"}, status=404)
 
-        # Pobranie 3 ostatnio dodanych pokoi dla danego akademika
         rooms = Room.objects.filter(dormitory_id=dormitory).order_by('-id')[:3]
         rooms_list = [
             {
@@ -49,7 +48,6 @@ def get_latest_rooms_by_dormitory(request, dormitory_id):
         ]
         return JsonResponse(rooms_list, safe=False)
     return JsonResponse({"error": "Invalid request method"}, status=400)
-
 
 
 
@@ -159,3 +157,58 @@ def get_room_status_by_dormitory(request, dormitory_id):
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
     return JsonResponse({"error": "Invalid request method"}, status=400)
+
+def edit_room(request, room_id):
+    if request.method == 'PUT':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+
+            try:
+                room = Room.objects.get(id=room_id)
+            except Room.DoesNotExist:
+                return JsonResponse({"error": f"Pokój o ID {room_id} nie istnieje."}, status=404)
+
+            dormitory = room.dormitory_id
+
+            new_room_number = data.get('room_number', room.room_number)
+            new_capacity = data.get('capacity', room.capacity)
+            new_type = data.get('type', room.type)
+            new_floor = data.get('floor', room.floor)
+            new_rent_cost = data.get('rent_cost', room.rent_cost)
+
+            if Room.objects.filter(
+                Q(dormitory_id=dormitory) & Q(room_number=new_room_number) & ~Q(id=room.id)
+            ).exists():
+                return JsonResponse(
+                    {"error": f"Pokój o numerze {new_room_number} już istnieje w tym akademiku."},
+                    status=400
+                )
+
+            if room.tenant_count > new_capacity:
+                return JsonResponse(
+                    {"error": "Pojemność nie może być mniejsza niż obecna liczba mieszkańców."},
+                    status=400
+                )
+
+            with transaction.atomic():
+                dormitory.capacity -= room.capacity
+                dormitory.capacity += new_capacity
+                dormitory.save()
+
+                room.room_number = new_room_number
+                room.capacity = new_capacity
+                room.type = new_type
+                room.floor = new_floor
+                room.rent_cost = new_rent_cost
+                room.full_clean()
+                room.save()
+
+            return JsonResponse({"message": f"Pokój o ID {room_id} został pomyślnie zaktualizowany."}, status=200)
+
+        except ValidationError as e:
+            return JsonResponse({"error": e.message_dict}, status=400)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"error": "Invalid request method"}, status=400)
+
