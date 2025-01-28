ba4459c61ea125dc271d45a8ab6d96b9e60b5923
Kamil Cichecki <kamil.cichecki00@gmail.com>
2025-01-29 00:12:48 +0100

    create_payment

--- Dormify/DormifyApp/views/Payment/payment_views.py
+++ Dormify/DormifyApp/views/Payment/payment_views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from DormifyApp.models import Payment, User
 import json
+from django.utils.timezone import now
 
 #manager ma widok ze wszystkimi uzytkownikami w zakładce payments - wyswietla mu się lista uzytkownikow i po prawo ma przycisk do zatwierdzania płatności. 
 #w momencie gdy przejdzie przelew od uzytkownika X, znajduje go na liście i klika przycisk by zatwierdzić płatność od danego mieszkańca. Wtedy tworzy się obiekt typu Payment
@@ -8,39 +9,38 @@ import json
 def create_payment(request):
     if request.method == 'POST':
         try:
-            # Pobierz dane z żądania
             data = json.loads(request.body.decode('utf-8'))
             user_id = data.get('user_id')
 
-            # Znajdź użytkownika
             try:
                 user = User.objects.get(id=user_id)
             except User.DoesNotExist:
                 return JsonResponse({"error": "Użytkownik o podanym ID nie istnieje."}, status=404)
 
-            # Sprawdź, czy użytkownik ma przypisany pokój
             if not user.room_id:
                 return JsonResponse({"error": "Użytkownik nie jest przypisany do żadnego pokoju."}, status=400)
 
-            # Pobierz szczegóły pokoju i koszt wynajmu
             room = user.room_id
             rent_cost = room.rent_cost
 
-            # Utwórz obiekt Payment
             payment = Payment.objects.create(
                 dormitory_id=user.dormitory_id,
                 user_id=user,
                 amount=rent_cost,
-                status=False,
+                status=True,
                 date_payment=now().date()
             )
 
+            user.is_payment_paid = True
+            user.save()
+
             return JsonResponse({
-                "message": "Płatność została pomyślnie utworzona.",
+                "message": "Płatność została pomyślnie utworzona i oznaczona jako opłacona.",
                 "payment_id": payment.id,
                 "user_id": user.id,
                 "amount": str(payment.amount),
-                "date_payment": payment.date_payment
+                "date_payment": payment.date_payment,
+                "is_payment_paid": user.is_payment_paid
             }, status=201)
 
         except Exception as e:
